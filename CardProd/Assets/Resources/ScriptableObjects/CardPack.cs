4b6b170cb9a3e50a43e27677c09bd7c3c91ff2a8
poponin07 <maximp95@mail.ru>
2023-01-27 18:06:31 +0300

    27.01

---
+++ CardProd/Assets/Resources/ScriptableObjects/CardPack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using OneLine;
+using UnityEngine;
+
+namespace Cards.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "CardPack", menuName = "CreateCardPack", order = 0)]
+    public class CardPack : ScriptableObject
+    {
+        [SerializeField, OneLine(Header = LineHeader.Short)]
+        public List<CardPropertiesData> ids = new List<CardPropertiesData>();
+
+        public CardPropertiesData GetCardById(uint id)
+        {
+            CardPropertiesData card = ids.Find(c => c.Id == id);
+            return card;
+        }
+    }
+}
---
+++ CardProd/Assets/Resources/ScriptableObjects/ScriptableObjectLoader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Cards.ScriptableObjects
+{
+    public class ScriptableObjectLoader : MonoBehaviour
+    {
+        private CardPack m_cardPack;
+        public const string SCRIPTABLE_OBJECTS_PATH = "ScriptableObjects/";
+        private void Awake()
+        {
+            m_cardPack = Resources.Load<CardPack>(SCRIPTABLE_OBJECTS_PATH + "CardPack");
+            string constName = "-CommonCardPack";
+            List<CardPackConfiguration> cardPacks = new List<CardPackConfiguration>(); 
+            for (int i = 1; i <= 7; i++)
+            {
+                cardPacks.Add(Resources.Load<CardPackConfiguration>(SCRIPTABLE_OBJECTS_PATH + i + constName));
+            }
+
+            for (int i = 0; i < cardPacks.Count; i++)
+            {
+                var cardPack = cardPacks[i];
+                m_cardPack.ids.AddRange(cardPack._cards);
+            }
+            CardPropertiesData card = m_cardPack.GetCardById(0);
+        }
+    }
+}
